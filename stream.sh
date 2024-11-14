475186bb6b00043a027b7a1369396920430702e7
Halffd <guhalfeld@hotmail.com>
2024-11-14 04:26:04 -0300

    reading from clipbord

--- stream.sh
+++ stream.sh
@@ -2,12 +2,10 @@
 
 # Check if a URL is provided
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <url>"
-    exit 1
+    URL=$(xclip -o)
+else
+    URL="$1"
 fi
-
-# Assign the first argument to the URL variable
-URL=$1
 
 # Run streamlink in the background and redirect output to /dev/null
 nohup streamlink --player="mpv" $URL best > /dev/null 2>&1 &
